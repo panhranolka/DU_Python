2f3c8f4ab3a84c3435ba55055918cc4f0c64cf03
panhranolka <tomaas.mt@gmail.com>
2025-09-30 21:51:02 +0200

    Delete 10.py

--- 10.py
+++
@@ -1,8 +0,0 @@
-# Naprogramuj funkciu, ktorá zistí, či je dané číslo párne.
-x = int(input("Zadaj číslo "))
-def parne(cislo):
-    if cislo % 2 == 0:
-        return "číslo je párne"
-    else:
-        return "číslo je nepárne"
-print (parne(x))
